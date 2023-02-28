32d132af76018921dd639d55c30ab963c0f7388d
eetzuh <irenakandic9@gmail.com>
2023-02-28 13:44:26 +0100

    New header design

--- mainpage.php
+++ mainpage.php
@@ -19,19 +19,11 @@ include "./getData.php"
 
 <body>
     <header class='sticky-top'>
-        <div class='menuContainer'>
-            <hr class="hrTop">
             <div class="menu">
                 <div class="homepage">
                     <a class="btn btn-light" href='./mainpage.php' ;>Homepage</a>
                 </div>
                 <div class="searchDiv">
-                    <!-- add theme switch -->
-                    <!-- <button class='theme'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-sun-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-                        </svg></button> -->
                     <form action="./mainpage.php" method="get">
                         <input class="searchField" type="text" name="term">
                         <button type="submit" class='searchButton'><svg xmlns="http://www.w3.org/2000/svg" width="16"
@@ -44,8 +36,6 @@ include "./getData.php"
                     <a class="btn btn-light" href="./" class="logOut">Log Out</a>
                 </div>
             </div>
-            <hr class="hrBottom">
-        </div>
     </header>
 
 
@@ -187,6 +177,7 @@ include "./getData.php"
                 }
             }
         }
+        // add option to view full posts
         ?>
         <!-- MODAL -->
         <div class="modal fade" id="exampleModal" tabindex="-1" data-bs-backdrop="static"
